622ca3c47a5903500ebdbe4b15ea91ee22d4ab32
Franck <franck@templatery.com>
2026-03-10 15:21:55 +0100

    Fix Astro v5 slug extension, add account_id, install fast-xml-builder

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -13,7 +13,7 @@ export async function GET(context) {
       title: post.data.title,
       pubDate: post.data.pubDate,
       description: post.data.description,
-      link: `/blog/${post.id}/`,
+      link: `/blog/${post.id.replace(/\.mdx?$/, '')}/`,
     })),
     customData: `<language>en-us</language>`,
   });
